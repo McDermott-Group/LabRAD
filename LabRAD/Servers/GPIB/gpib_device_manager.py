211d377eb1891dd7474b003272ae8d010f14acb3
Ivan Pechenezhskiy <pechenezhski@wisc.edu>
2015-06-24 19:30:16 -0500

    remove unnecessary yield statements

--- LabRAD/Servers/GPIB/gpib_device_manager.py
+++ LabRAD/Servers/GPIB/gpib_device_manager.py
@@ -136,21 +136,20 @@ class GPIBDeviceManager(LabradServer):
         or the query fails, the name will be listed as '<unknown>'.
         """
         for cls_cmd, idn_cmd in [('*CLS', '*IDN?'), ('', 'ID?'), ('IP', 'OI')]:
-            p = yield self.client.servers[server].packet()
+            p = self.client.servers[server].packet()
             p.address(channel).timeout(Value(1,'s')).write(cls_cmd).query(idn_cmd)
             print("Sending '" + idn_cmd + "' to " + str(server) + " " + str(channel))
             resp = None
+            name = UNKNOWN
             try:
                 resp = (yield p.send()).query
-            except Exception, e:
-                name = UNKNOWN
+            except Exception:
                 print("No response to '" + idn_cmd + "' from " + str(server) + " " + str(channel))
                 continue
             name = parseIDNResponse(resp, idn_cmd)
-            if name != UNKNOWN:
-                print(str(server) + " " + str(channel) + " '" + idn_cmd + "' response: '" + resp + "'")
-                print(str(server) + " " + str(channel) + " device name: '" + name + "'")
-                break
+            print(str(server) + " " + str(channel) + " '" + idn_cmd + "' response: '" + resp + "'")
+            print(str(server) + " " + str(channel) + " device name: '" + name + "'")
+            break
         returnValue((name, resp))
 
     def identifyDevice(self, server, channel, idn):
