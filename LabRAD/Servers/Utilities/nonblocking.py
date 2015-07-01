397145e1c3ca89f258ffabab1db9c7722ffd8438
Ivan Pechenezhskiy <pechenezhski@wisc.edu>
2015-07-01 04:48:35 -0500

    Improve the sleep function documentation

--- LabRAD/Servers/Utilities/nonblocking.py
+++ LabRAD/Servers/Utilities/nonblocking.py
@@ -1,7 +1,12 @@
 from twisted.internet import reactor, defer
 
-def sleep(time_in_seconds):
-    """Use this function instead of time.sleep(time_in_seconds) in methods decorated with inlineCallbacks."""
+def sleep(time_in_seconds=1):
+    """This is non-blocking sleep function that could be used to delay the code execution 
+    by specified amount of time expressed in seconds. 
+    
+    For example, to add a non-blocking ten-second delay to your code, you can write `yield sleep(10)` 
+    inside a method that is decorated with `@inlineCallbacks`.
+    """
     d = defer.Deferred()
     reactor.callLater(time_in_seconds, d.callback, None)
     return d
