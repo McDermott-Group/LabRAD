a78a8d2b418baf7f27ca4e26a40782ebe0a5e3ee
Chris Wilen <cwilen@wisc.edu>
2015-08-03 17:58:43 -0500

    fixed adr1 startup script
    previously started adr2 instead

--- LabRAD/StartupScripts/ADR1_controller.bat
+++ LabRAD/StartupScripts/ADR1_controller.bat
@@ -3,5 +3,5 @@ timeout 3
 START "" twistd.py -n labradnode
 echo Please enter password in node window that pops up
 pause
-START "" ../Servers/ADR/ADRServer.py -a ADR2 -w ""
+START "" ../Servers/ADR/ADRServer.py -a ADR1 -w ""
 START "" ../Servers/ADR/ADRClient.py -w ""
