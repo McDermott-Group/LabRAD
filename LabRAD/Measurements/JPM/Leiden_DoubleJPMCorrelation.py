Fix DmodFreq units in the ADC board interface

LabRAD/Measurements/JPM/Leiden_DoubleJPMCorrelation.py
@@ -93,7 +93,7 @@ ExptInfo = {
 
 # Experiment Variables
 ExptVars = {
-            'Reps': 3000, # should not exceed ~50,000
+            'Reps': 25000, # should not exceed ~50,000
 
             'RF Frequency': 20 * GHz, #3.54 * GHz,
             'RF Power': -110 * dBm,
@@ -108,14 +108,14 @@ ExptVars = {
             'Bias Time': 150 * us,
             'Measure Time': 20 * us,
           
-            'JPM A Bias Voltage': .298 * V,
+            'JPM A Bias Voltage': .2842 * V,
             'JPM A Fast Pulse Time': 3 * ns,
-            'JPM A Fast Pulse Amplitude': .5 * DACUnits,
+            'JPM A Fast Pulse Amplitude': 1 * DACUnits,
             'JPM A Fast Pulse Width': 0 * ns,
             
-            'JPM B Bias Voltage': .191 * V,
+            'JPM B Bias Voltage': .1565 * V,
             'JPM B Fast Pulse Time': 3 * ns,
-            'JPM B Fast Pulse Amplitude': .5 * DACUnits,
+            'JPM B Fast Pulse Amplitude': 1 * DACUnits,
             'JPM B Fast Pulse Width': 0 * ns,
 
             # Both JPM A and JPM B fast pulses should appear within
@@ -133,11 +133,11 @@ ExptVars = {
 with double_jpm_experiments.DoubleJPMCorrelation() as run:
     run.set_experiment(ExptInfo, Resources, ExptVars) 
 
-    # run.sweep('JPM B Bias Voltage', np.linspace(.0, .25, 251) * V,
+    # run.sweep('JPM B Bias Voltage', np.linspace(.17, .18, 251) * V,
             # save=True, print_data=['Pa', 'Pb', 'P11'], plot_data=['Pa', 'Pb', 'P11'])
     
     # run.sweep([['JPM A Bias Voltage'], ['JPM B Bias Voltage']],
-            # [[np.linspace(.295, .32, 151) * V], [np.linspace(.172, .178, 151) * V]], 
+            # [[np.linspace(.275, .32, 51) * V], [np.linspace(0*.155, 0*.178, 51) * V]], 
             # save=True, print_data=['Pa', 'Pb', 'Temperature'], plot_data=['Pa', 'Pb'],
             # dependencies=[['Pa', 'JPM A Detection Time'], ['Pb', 'JPM B Detection Time']])
     
@@ -160,11 +160,11 @@ with double_jpm_experiments.DoubleJPMCorrelation() as run:
     # run.sweep('JPM A to JPM B Fast Pulse Delay', np.linspace(-10, 10, 21) * ns, 
         # save=True, print_data=['Pa', 'Pb', 'P11', 'Corr Coef', 'Temperature'], plot_data=['Pa', 'Pb', 'P11'])
     
-    # run.sweep('JPM A to JPM B Fast Pulse Delay', np.linspace(-500, 500, 126) * ns, 
+    # run.sweep('JPM A to JPM B Fast Pulse Delay', np.linspace(-50, 50, 51) * ns, 
             # save=True, print_data=['Pa', 'Pb', 'P11', 'Temperature'], plot_data=['Pa', 'Pb', 'P11'])
     
-    # run.sweep('JPM A to JPM B Fast Pulse Delay', np.linspace(-5000, 5000, 501) * ns, 
-            # save=True, print_data=['Pa', 'Pb', 'P11', 'Temperature'], plot_data=['Pa', 'Pb', 'P11'])
+    run.sweep('JPM A to JPM B Fast Pulse Delay', np.linspace(-5000, 5000, 1001) * ns, 
+            save=True, print_data=['Pa', 'Pb', 'P11', 'Temperature'], plot_data=['Pa', 'Pb', 'P11'])
     
     # run.value('Reps', 2500)
     # run.sweep('JPM A to JPM B Fast Pulse Delay', np.linspace(-500, 500, 51) * ns, 

LabRAD/Measurements/JPM/Leiden_HEMTCavityJPM.py
@@ -22,10 +22,12 @@ Resources = [ {
                                         'DAC B': 'None',
                                         'FO1 FastBias Firmware Version': '2.1',
                                         'FO2 FastBias Firmware Version': '2.1',
+                                        'Data': False,
                                        },
                 'Leiden Board DAC 4': {
                                         'DAC A': 'None',
                                         'DAC B': 'None',
+                                        'Data': False,
                                        },
                 'Leiden Board ADC 5': {
                                         'RunMode': 'demodulate', #'average'
@@ -37,7 +39,7 @@ Resources = [ {
                                         'DemodPhase': 0 * rad,
                                         'DemodCosAmp': 255,
                                         'DemodSinAmp': 255,
-                                        'DemodFreq': 0 * MHz,
+                                        'DemodFreq': -30 * MHz,
                                         'ADCDelay': 0 * ns,
                                         'Data': True
                                        },
@@ -49,13 +51,14 @@ Resources = [ {
                                 'Fast Pulse Time': {},
                                 'Fast Pulse Amplitude': {},
                                 'Fast Pulse Width': {'Value': 0 * ns},
+                                'RF SB Frequency': {'Value': 30 * MHz},
                                 'ADC Wait Time': {'Value': 0 * ns}
                              }
                 },
-                { # GPIB RF Generator
+                { # GPIB RF Generator.
                     'Interface': 'RF Generator',
                     'Address': comp_name + ' GPIB Bus - GPIB0::20::INSTR',
-                    'Variables': {  
+                    'Variables': {
                                     'RF Power': {'Setting': 'Power'}, 
                                     'RF Frequency': {'Setting': 'Frequency'}
                                  }
@@ -88,39 +91,40 @@ Resources = [ {
 ExptInfo = {
             'Device Name': 'MH048B-051215A-D10',
             'User': 'Ivan Pechenezhskiy',
-            'Base Path': 'Z:\mcdermott-group\Data\Leiden DR 2015-09-25 - Qubits and JPMs',
+            'Base Path': r'Z:\mcdermott-group\Data\Matched JPM Photon Counting\Leiden DR 2015-10-02 - Cavity Excitation by JPM',
             'Experiment Name': 'InverseDirectionCavitySpectroscopy',
-            'Comments': 'Driving qubit cavity through JPM Fast Pulse line.' 
+            'Comments': 'MH048B Qubit and 051215A-D10 JPM in a single box. Driving qubit cavity through JPM Fast Pulse line.' 
            }
  
 # Experiment Variables
 ExptVars = {
-            'Reps': 5000, # should not exceed ~50,000
+            'Reps': 25000, # should not exceed ~50,000
 
             'Qubit Flux Bias Voltage': 0 * V,
 
             'RF Frequency': 4.821 * GHz,
+            'RF SB Frequency': 30 * MHz,
             'RF Power': 13 * dBm,
 
-            'Init Time': 100 * us,
+            'Init Time': 50 * us,
             'Bias Time': 100 * us,
-            'Measure Time': 50 * us,
+            'Measure Time': 75 * us,
           
-            'Bias Voltage': 0.195 * V,
+            'Bias Voltage': 0.184 * V,
             'Fast Pulse Time': 10 * ns,
-            'Fast Pulse Amplitude': .1492 * DACUnits,
+            'Fast Pulse Amplitude': .5 * DACUnits,
             'Fast Pulse Width': 0 * ns,
 
-            'ADC Wait Time': 1000 * ns, # time delay between the start of the readout pulse and the start of the demodulation
+            'ADC Wait Time': 10 * ns, # time delay between the start of the readout pulse and the start of the demodulation
            }
 
 with hemt_qubit_experiments.HEMTCavityJPM() as run:
     
     run.set_experiment(ExptInfo, Resources, ExptVars)
 
-    run.single_shot_iqs(save=False, plot_data=True)
+    # run.single_shot_iqs(save=False, plot_data=True)
     # run.single_shot_osc(save=False, plot_data=['I', 'Q'])
-    # run.avg_osc(save=True, plot_data=['I', 'Q'], runs=250)
+    # run.avg_osc(save=False, plot_data=['I', 'Q'], runs=250)
 
-    # run.sweep('Readout Frequency', np.linspace(4.65, 4.85, 201) * GHz,
-              # plot_data=['I', 'Q', 'ADC Amplitude'], save=True)
+    run.sweep('RF Frequency', np.linspace(4.50, 5.00, 501) * GHz,
+              plot_data=['I', 'Q', 'Mean ADC Amplitude'], save=True, max_data_dim=1)

LabRAD/Measurements/JPM/hemt_qubit_experiments.py
@@ -84,8 +84,8 @@ class HEMTExperiment(expt.Experiment):
 
     def avg_osc(self, adc=None, save=False, plot_data=None, runs=100):
         """
-        Run a single experiment in average mode Reps number of times 
-        and average the results together.
+        Run a single experiment in average mode specified number of
+        times and average the results together.
         
         Inputs:
             adc: ADC board name.
@@ -423,7 +423,8 @@ class HEMTCavityJPM(HEMTExperiment):
         ###SET BOARDS PROPERLY#####################################################################
         self.boards.set_adc_setting('ADCDelay', (DAC_ZERO_PAD_LEN +
                 ADC_wait_time) * units.ns, adc)
-
+        self.boards.set_adc_setting('DemodFreq', -self.value('RF SB Frequency'), adc)
+        
         # Create a memory command list.
         # The format is described in Servers.Instruments.GHzBoards.command_sequences.
         mem_lists = self.boards.init_mem_lists()
@@ -448,9 +449,9 @@ class HEMTCavityJPM(HEMTExperiment):
         self.acknowledge_requests()
         self.send_request('Temperature')
         result = self.boards.load_and_run(dac_srams, mems, self.value('Reps'))
-        
+
         ###DATA POST-PROCESSING####################################################################
-        Is, Qs = result[0] 
+        Is, Qs = result[0]
         Is = np.array(Is)
         Qs = np.array(Qs)
 
@@ -495,11 +496,38 @@ class HEMTCavityJPM(HEMTExperiment):
                         'Value': self.acknowledge_request('Temperature')}
                    }
         elif self.boards.get_adc_setting('RunMode', adc) == 'average':
-            raise NotImplementedError("ADC mode 'average' is not " +
-                "supported for this specific experiment to avoid" +
-                "misinterpretation of the results.")
+            self.value('Reps', 1, output=False)
+            time = np.linspace(0, 2 * (len(Is) - 1), len(Is))
+            I, Q = data_processing.software_demod(time, 0, Is, Qs)
+            return {
+                    'I': { 
+                        'Value': Is * units.ADCUnits,
+                        'Dependencies': ['ADC Time'],
+                        'Preferences':  {'linestyle': 'b-'}},
+                    'Q': { 
+                        'Value': Qs * units.ADCUnits,
+                        'Dependencies': ['ADC Time'],
+                        'Preferences':  {'linestyle': 'g-'}},
+                    'Software Demod I': { 
+                        'Value': I * units.ADCUnits,
+                        'Preferences':  {'linestyle': 'b.'}},
+                    'Software Demod Q': { 
+                        'Value': Q * units.ADCUnits,
+                        'Preferences':  {'linestyle': 'g.'}}, 
+                    'Software Demod ADC Amplitude': { 
+                        'Value': np.sqrt(I**2 + Q**2) * units.ADCUnits,
+                        'Preferences':  {'linestyle': 'r.'}},
+                    'Software Demod ADC Phase': { 
+                        'Value': np.arctan2(Q, I) * units.rad,
+                        'Preferences':  {'linestyle': 'k.'}},
+                    'ADC Time': {
+                        'Value': time * units.ns,
+                        'Type': 'Independent'},
+                    'Temperature': {
+                        'Value': self.acknowledge_request('Temperature')}
+                   }
 
     def average_data(self, data):
         raise NotImplementedError("Proper averaging of multiple runs " +
             "is not implemented for this specific experiment to avoid" +
-            "misinterpretation of the results.")
+            " any misinterpretation of the results.")

LabRAD/Measurements/JPM/jpm_qubit_experiments.py
@@ -58,7 +58,7 @@ class JPMExperiment(expt.Experiment):
         elif number_of_devices > 2:
             raise Exception('Histogram plotting for more than two ' +
             'devices is not implemented.')
-        plt.xlabel('Timing Information [PreAmpTimeCounts]')
+        plt.xlabel('Timing Information [Preamp Time Counts]')
         plt.ylabel('Counts')
         plt.xlim(0, preamp_timeout) 
         plt.draw()
@@ -159,7 +159,7 @@ class JPMQubitReadout(JPMExperiment):
         if 'None' in self.boards.requested_waveforms:
             waveforms['None'] = np.hstack([pulse.DC(DAC_ZERO_PAD_LEN + QB_time + QBtoRO + RO_time + ROtoD + Disp_time + FPtoEnd, 0)])
         
-        if 'JPM Fast Pulse' in requested_waveforms:
+        if 'JPM Fast Pulse' in self.boards.requested_waveforms:
             waveforms['JPM Fast Pulse'] = np.hstack([pulse.DC(DAC_ZERO_PAD_LEN + QB_time + QBtoRO + RO_time + ROtoD + Disp_time + DtoFP, 0),
                                                      JPM_smoothed_FP,
                                                      pulse.DC(max(0, -DtoFP - JPM_smoothed_FP.size) + DAC_ZERO_PAD_LEN, 0)])      
@@ -254,7 +254,7 @@ class JPMQubitReadout(JPMExperiment):
         ###DATA STRUCTURE##########################################################################
         return {
                 'Switching Probability': {
-                    'Value': dp.prob(P, 0, max_threshold),
+                    'Value': dp.prob(P, 0, threshold),
                     'Distribution': 'binomial',
                     'Preferences':  {
                         'linestyle': 'b-',