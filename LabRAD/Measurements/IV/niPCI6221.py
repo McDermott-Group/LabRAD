13c33ab1e52ff9c88c42c77640e1e917775832cf
Chris Wilen <cwilen@wisc.edu>
2015-11-17 22:38:07 -0600

    Added a working version of niPCI6221.py that synchronizes AO/AI on the national instruments PCI 6221 cards (formerly known BNC 2110). Synchronized IV Sweeps on the way!!

--- LabRAD/Measurements/IV/niPCI6221.py
+++ LabRAD/Measurements/IV/niPCI6221.py
@@ -6,61 +6,81 @@ from PyDAQmx.DAQmxCallBack import *
 import numpy as np
 import ctypes
 import re
+import time
 
 NI_PCI_6221_MAX_VOLTAGE = 10.0;  # Max voltage output
 NI_PCI_6221_MAX_SAMP_RATE = 10000;   # Measurement/generation rate ?? double check this
 
-class NIPCI6221(object):
-    """ This Class is a set of methods for controlling the NI PCI 6221 boards."""
+trigName = ""
+
+class CallbackTask(Task):
     def __init__(self):
-        pass
+        Task.__init__(self)
+        self.buffLen = 0
+        self.callback =None
+        
+    def configureCallbackTask(self, analogInputNameStr, sampRate, numSamples):
+        self.buffLen = numSamples
+        self.data = np.zeros(self.buffLen)
+        self.CreateAIVoltageChan(analogInputNameStr,"",DAQmx_Val_Diff,-10.0,10.0,DAQmx_Val_Volts,None)
+        self.CfgSampClkTiming("",float(sampRate),DAQmx_Val_Rising,DAQmx_Val_ContSamps,numSamples)
+        trigName = self.GetTerminalNameWithDevPrefix("ai/StartTrigger")
+        self.AutoRegisterEveryNSamplesEvent(DAQmx_Val_Acquired_Into_Buffer,1000,0)
+        self.AutoRegisterDoneEvent(0)
+        
+    def EveryNCallback(self):
+        numReadBack = int32()
+        self.data=np.zeros(self.buffLen)
+        self.ReadAnalogF64(self.buffLen,10.0,DAQmx_Val_GroupByChannel,self.data,self.buffLen,byref(numReadBack),None)
+        if self.callback is not None:
+            self.callback(self.data)
+        return 0 # The function should return an integer
+    
+    def DoneCallback(self, status):
+        print "Status",status.value
+        return 0 # The function should return an integer
 
-    def configureSynchronizedAIAOTask(self, analogInput, acAnalogOutput, dcAnalogOutput, sampRate, readBuffer, writeBuffer, dcValue):
-        try:
-            aiTaskHandle = TaskHandle()
-            acTaskHandle = TaskHandle()
-            dcTaskHandle = TaskHandle()
-
-            # DAQmx Configure Analog Input
-            if re.match("Dev1/ai\d", element):
-                DAQmxCreateTask("",byref(aiTaskHandle))
-                DAQmxCreateAIVoltageChan(aiTaskHandle,"Dev1/ai1","",DAQmx_Val_Diff,-10.0,10.0,DAQmx_Val_Volts,None)
-                DAQmxCfgSampClkTiming(aiTaskHandle,"",10000.0,DAQmx_Val_Rising,DAQmx_Val_ContSamps,buffsize)
-            else:
-                print "Analog inputs by the name of ", analogInput, " are not supported by this instrument"
-
-            trigName = self.GetTerminalNameWithDevPrefix(aiTaskHandle, "ai/StartTrigger")
-            print "trigName=", trigName
-
-            #DAQmx Configure AC Analog Output
-            if re.match("Dev1/ao\d", element):
-                DAQmxCreateTask("",byref(acTaskHandle))
-                DAQmxCreateAOVoltageChan(acTaskHandle,"Dev1/ao0","",-10.0,10.0,DAQmx_Val_Volts,None)
-                DAQmxCfgSampClkTiming(acTaskHandle, "", sr/2,DAQmx_Val_Rising,DAQmx_Val_ContSamps,buffsize)
-                DAQmxCfgDigEdgeStartTrig(acTaskHandle,trigName,DAQmx_Val_Rising)
-            else:
-                print "Analog outputs by the name of ", analogOutput, " are not supported by this instrument"
-
-            DAQmxStopTask(aiTaskHandle)
-            DAQmxClearTask(aiTaskHandle)
-            DAQmxStopTask(acTaskHandle)
-            DAQmxClearTask(acTaskHandle)
-        except DAQError as err:
-            print "DAQmx Error: %s"%err
-
-    def GetTerminalNameWithDevPrefix(self, taskHandle, terminalName):
+    def setCallback(self, function):
+        self.callback = function
+    
+    def GetTerminalNameWithDevPrefix(self, terminalName):
         device = ctypes.create_string_buffer(256)
         productCategory = int32()
 	numDevices = uInt32()
 	i = 1
-	try:
-            DAQmxGetTaskNumDevices(taskHandle,byref(numDevices))
-            for i in range(1, numDevices.value+1):
-                DAQmxGetNthTaskDevice(taskHandle,i,device,256)
-                DAQmxGetDevProductCategory(device,byref(productCategory))
-                if productCategory.value!=DAQmx_Val_CSeriesModule and productCategory.value!=DAQmx_Val_SCXIModule:
-                    triggername = "/" + device.value + "/" + terminalName
-                    break
-        except DAQError as err:
-            print "DAQmx Error: %s"%err
+        self.GetTaskNumDevices(byref(numDevices))
+        for i in range(1, numDevices.value+1):
+            self.GetNthTaskDevice(i,device,256)
+            DAQmxGetDevProductCategory(device,byref(productCategory))
+            if productCategory.value!=DAQmx_Val_CSeriesModule and productCategory.value!=DAQmx_Val_SCXIModule:
+                triggername = "/" + device.value + "/" + terminalName
+                break
+        return triggername
+
+class AnalogOuputTask(Task):
+    def __init__(self):
+        Task.__init__(self)
+        self.buffLen = 0
+        
+    def configureAnalogOutputTask(self, analogOutputNameStr, outputRate, outWaveForm):
+        self.buffLen = len(outWaveForm)
+        self.CreateAOVoltageChan(analogOutputNameStr,"",-10.0,10.0,DAQmx_Val_Volts,None)
+        self.CfgSampClkTiming("", outputRate,DAQmx_Val_Rising,DAQmx_Val_ContSamps,self.buffLen)
+        trigName = "/Dev1/ai/StartTrigger" # pass from one to the other !!!!!!!!!!!!
+        self.CfgDigEdgeStartTrig(trigName,DAQmx_Val_Rising)
+        sampsPerChanWritten = int32()
+        self.WriteAnalogF64(self.buffLen, False, 10.0, DAQmx_Val_GroupByChannel, outWaveForm, byref(sampsPerChanWritten), None)
+
+    def GetTerminalNameWithDevPrefix(self, terminalName):
+        device = ctypes.create_string_buffer(256)
+        productCategory = int32()
+	numDevices = uInt32()
+	i = 1
+        self.GetTaskNumDevices(byref(numDevices))
+        for i in range(1, numDevices.value+1):
+            self.GetNthTaskDevice(i,device,256)
+            DAQmxGetDevProductCategory(device,byref(productCategory))
+            if productCategory.value!=DAQmx_Val_CSeriesModule and productCategory.value!=DAQmx_Val_SCXIModule:
+                triggername = "/" + device.value + "/" + terminalName
+                break
         return triggername
--- LabRAD/Measurements/IV/test.py
+++ LabRAD/Measurements/IV/test.py
@@ -1,130 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 07 12:58:41 2013
-@author: Pete
-"""
-import daqmx
+import niPCI6221 as ni
 import numpy as np
-import matplotlib.pyplot as plt
 import time
+import matplotlib.pyplot as plt
 
-filename = r"C:\NIData\test.npy"
-np.save(filename, np.array([]))
-
-numperiods = 3
-period = 2.0
-buffsize = 1000
-sr = buffsize/period
-
-t = np.linspace(0, 4*np.pi, buffsize)
-dataw = 2*np.sin(t) #+ 0.5*np.random.random(len(t))
-
-
-AItaskHandle = daqmx.TaskHandle()
-AOtaskHandle = daqmx.TaskHandle()
-
-
-
-daqmx.CreateTask("", AItaskHandle)
-#CreateTask creates NI task object
-daqmx.CreateAIVoltageChan(AItaskHandle, "Dev1/ai1", "", daqmx.Val_Diff,
-                          -10.0, 10.0, daqmx.Val_Volts, None)
-#CreateAIVoltageChan(taskHandle,physicalChannel,nameToAssignToChannel,terminalConfig,minVal,maxVal,units,customScaleName)
-daqmx.CfgSampClkTiming(AItaskHandle, "", sr, daqmx.Val_Rising, 
-                       daqmx.Val_ContSamps, buffsize)
-#CfgSampClkTiming(taskHandle,source,rate,activeEdge,sampleMode,sampsPerChanToAcquire)
-trigName = daqmx.GetTerminalNameWithDevPrefix(AItaskHandle, "ai/StartTrigger")
-# e.g. trigName="/Dev1/ai/StartTrigger"
-print "trigName=", trigName
-
-
-
-
-daqmx.CreateTask("", AOtaskHandle)
-daqmx.CreateAOVoltageChan(AOtaskHandle, "Dev1/ao0", "", 
-                          -10.0, 10.0, daqmx.Val_Volts, None)
-#CreateAOVoltageChan(taskHandle,physicalChannel,nameToAssignToChannel,minVal,maxVal,units,customScaleName)
-daqmx.CfgSampClkTiming(AOtaskHandle, "", sr/2, daqmx.Val_Rising, 
-                       daqmx.Val_ContSamps, buffsize)
-#CfgSampClkTiming(taskHandle,source,rate,activeEdge,sampleMode,sampsPerChanToAcquire)
-# i.e on Dev1, analog output 0, output at sr/2 samples per second, start on internal clock rising, acquire indefinitel until stopTask, 
-daqmx.CfgDigEdgeStartTrig(AOtaskHandle, trigName, daqmx.Val_Rising)
-print "CfgDigEdgeStartTrig"
-
-
-
-class MyList(list):
-    pass
-
-# list where the data are stored
-data = MyList()
-id_data = daqmx.create_callbackdata_id(data)
-print "id_data=", id_data
-
-def EveryNCallback_py(taskHandle, everyNsamplesEventType, nSamples, 
-                      callbackData_ptr):
-                          
-    callbackdata = daqmx.get_callbackdata_from_id(callbackData_ptr)
-    
-    data, npoints = daqmx.ReadAnalogF64(taskHandle, buffsize, 10.0, 
-                                        daqmx.Val_GroupByChannel, buffsize, 1)
-                       
-    callbackdata.extend(data.tolist())
-    print "Acquired %d samples"%len(data)
-    saved_data = np.load(filename)
-    new_saved_data = np.append(saved_data, np.asarray(data))
-    np.save(filename, new_saved_data)
-    return 0 # The function should return an integer
-    
-# Convert the python function to a CFunction
-EveryNCallback = daqmx.EveryNSamplesEventCallbackPtr(EveryNCallback_py)
-print "EveryNCallback"
-
-daqmx.RegisterEveryNSamplesEvent(AItaskHandle,daqmx.Val_Acquired_Into_Buffer,
-                                 buffsize, 0, EveryNCallback, id_data)
+def CallMe(data):
+    print "CallMe"
+    #plt.plot(data)
+    #plt.ylabel('time averaged')
+    #plt.show()
     
 
 
+readBuf = np.array([])
+t = np.linspace(0, 2*np.pi, 1000)
+writeBuf = 2*np.sin(t)
+writeBuf.astype(np.float64)
 
-def DoneCallback_py(taskHandle, status, callbackData_ptr):
-    print "Status", status.value
-    return 0
-    
-DoneCallback = daqmx.DoneEventCallbackPtr(DoneCallback_py)
-
-daqmx.RegisterDoneEvent(AItaskHandle, 0, DoneCallback, None)
-
-
-written = daqmx.WriteAnalogF64(AOtaskHandle, buffsize, False, 10.0, 
-                               daqmx.Val_GroupByChannel, dataw)
-                               
-                               
-                     
-daqmx.StartTask(AOtaskHandle)
-daqmx.StartTask(AItaskHandle)
+inp = ni.CallbackTask()
+inp.configureCallbackTask("Dev1/ai0", 1000.0, 1000)
+inp.setCallback(CallMe)
 
 
-t0 = time.time()
-elapsed = 0.0
+out = ni.AnalogOuputTask()
+out.configureAnalogOutputTask("Dev1/ao0", 1000.0, writeBuf)
 
-while elapsed < period*numperiods:
-    elapsed = time.time() - t0
+out.StartTask()
+inp.StartTask()
+
+raw_input('Acquiring samples continuously. Press Enter to interrupt\n')
+
+inp.StopTask()
+inp.ClearTask()
+out.StopTask()
+out.ClearTask()
 
 
-daqmx.StopTask(AItaskHandle)
-daqmx.ClearTask(AItaskHandle)
-daqmx.StopTask(AOtaskHandle)
-daqmx.ClearTask(AOtaskHandle)
 
-print "ClearTask"
-
-plt.close('all')
-t = np.arange(0, 1/sr*len(data), 1/sr)
-plt.plot(t, data)
-plt.ylabel("V (t)")
-plt.xlabel("t (s)")
-plt.hold(True)
-plt.show()
-#saved_data = np.load(filename)
-#plt.plot(t, saved_data, '--r')
-print "End"
